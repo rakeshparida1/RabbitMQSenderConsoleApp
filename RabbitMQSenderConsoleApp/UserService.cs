15e1114318ee8334dd5974dfd9240d8ee3fd5478
HY412DD <mrparida.rakesh@gmail.com>
2022-06-29 11:51:30 +0530

    Add project files.

---
+++ RabbitMQSenderConsoleApp/UserService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace RabbitMQSenderConsoleApp
+{
+    internal class UserService
+    {
+        public UserService()
+        {
+        }
+
+        internal List<User> GetAllUsersToSend()
+        {
+            List<User> userList = new List<User>();
+            userList.Add(new User { FirstName = "rakesh", LastName = "kumar", EmailAddress = "abc@gmail.com" });
+            userList.Add(new User { FirstName = "rakesh", LastName = "sharma", EmailAddress = "abc2@outsmarthub.co.nz" });
+
+            return userList;
+        }
+    }
+}
